332aecdb179cb800eadc14c769487c7dc1976109
Bohdlesk <bohdanleskovets@gmail.com>
2021-04-23 17:12:07 +0300

    token check middleware added

---
+++ src/middleware/accessChecker.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-underscore-dangle */
+import { NextFunction, Request, Response } from 'express';
+
+import { TokenModel } from '../models/tokens/tokens.model';
+
+export default async (req: Request, res:Response, next: NextFunction): Promise<void > => {
+  try {
+    console.log(req.headers.authorization);
+    const token = await TokenModel.findOne({ token: req.headers.authorization });
+    if (token) {
+      console.log('access passed');
+      req.headers._idSystemUser = token._idSystemUser;
+      next();
+    } else {
+      console.log('access failed');
+      res.status(401).json({
+        message: 'Failed to authorize',
+      });
+    }
+  } catch (error) {
+    res.status(401).json({
+      message: 'Failed to authorize',
+      error,
+    });
+  }
+};
