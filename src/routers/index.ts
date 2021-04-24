6090c38195e6309f588f4230dffa64fe789fe4c8
Bohdlesk <bohdanleskovets@gmail.com>
2021-04-24 21:50:59 +0300

    lint code

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import { tasksRouter } from './tasks';
-import { usersRouter } from './users';
 
 const APIV1Router = Router();
 
 APIV1Router.use('/tasks', tasksRouter);
-// APIV1Router.use('/users', usersRouter);
 
 export { APIV1Router };
