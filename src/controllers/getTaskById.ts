a0bbdf13959b9c12fbd82011b9f3721c7c0bcf47
Bohdlesk <bohdanleskovets@gmail.com>
2021-04-24 21:37:31 +0300

    added check check for task existence

--- src/controllers/getTaskById.ts
+++ src/controllers/getTaskById.ts
@@ -6,9 +6,15 @@ export default async (req: Request, res: Response): Promise<void> => {
   try {
     if (req.headers.systemFunctionIds && req.headers.systemFunctionIds.includes('2')) {
       const task = await TaskModel.findOne({ _id: req.params._id });
-      res.status(200).json({
-        task,
-      });
+      if (task) {
+        res.status(200).json({
+          task,
+        });
+      } else {
+        res.status(404).json({
+          status: 'Task is not found',
+        });
+      }
     } else {
       res.status(401).json({
         message: 'System function is not allowed',
--- src/controllers/getTaskByUserId.ts
+++ src/controllers/getTaskByUserId.ts
@@ -6,9 +6,15 @@ export default async (req: Request, res: Response): Promise<void> => {
   try {
     if (req.headers.systemFunctionIds && req.headers.systemFunctionIds.includes('4')) {
       const task = await TaskModel.findOne({ _idSystemUser: String(req.query._id) });
-      res.status(200).json({
-        task,
-      });
+      if (task) {
+        res.status(200).json({
+          task,
+        });
+      } else {
+        res.status(404).json({
+          status: 'Task is not found',
+        });
+      }
     } else {
       res.status(500).json({
         status: 'System function is not allowed',
